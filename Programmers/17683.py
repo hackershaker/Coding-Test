a3d139bc9e55630c86b5bf436c4077195f777385
unknown <deltahackall@gmail.com>
2022-10-06 23:10:19 +0900

    in progress

--- Programmers/17683.py
+++ Programmers/17683.py
@@ -10,13 +10,32 @@ def solution(m, musicinfos):
         else: playtime = 60 - int(starttime[3:]) + 60 * ( int(endtime[:2]) - int(starttime[:2])-1 ) + int(endtime[3:])
         # print(playtime)
         temp = ""
-        while True:
-            if len(temp + sheet) > playtime:
-                temp += sheet;break
-            else: temp += sheet
+        lensheet = len(sheet) - sheet.count("#")
+        for _ in range(int(playtime/lensheet)): temp += sheet
+        c = 0; i=0
+        while c < playtime % lensheet:
+            try:
+                if sheet[i].isalpha() and sheet[i+1] == "#": 
+                    temp += sheet[i:i+2]
+                    c += 1
+                    i += 1
+                else:
+                    temp += sheet[i]
+                    c += 1
+                    i += 1
+            except:
+                temp += sheet[i]
+                c += 1
+
         sheet = temp
         # print(sheet)
-        if re.search(f"{m}[^#]", sheet) != None: answer.append([name, playtime])
+
+        if re.search(f"{m}[A-Z]", sheet) != None:
+            # print("Gotcha")
+            answer.append([name, playtime])
+        else:
+            if re.search(f"{m}$", sheet) != None:
+                answer.append([name, playtime])
 
     # print(answer)
     if answer: return sorted(answer, key=lambda x: -x[1])[0][0]
@@ -33,4 +52,8 @@ print(solution(	"A", ["22:00,23:00,HELLO,CDEFGAB", "22:00,23:00,WORLD,ABCDEF", "
 print(solution(	"ABC", ["12:00,12:06,HELLO,DEFABC", "12:00,12:06,12:12,WORLD,ABABC#"]), "HELLO")
 print(solution(	"F#D#", ["16:23,23:50,HELLO,C#AF#DEF#C#D#", "22:15,23:59,WORLD,F#C#D#A#F#A#D#D#F"]), "(None)")
 print(solution(	"CC#C", ["16:23,23:50,HELLO,C#C#CC#", "22:15,23:59,WORLD,C#CC#C#C#"]), "(None)")
-print(solution(	"CCC#", ["16:23,23:50,HELLO,CC#C#C", "22:15,23:59,WORLD,C#C#C"]), "HELLO")
+print(solution(	"CCC#", ["00:50,23:50,HELLO,CC#C#C", "12:15,23:01,WORLD,C#C#C"]), "HELLO")
+print(solution(	"CC#", ["16:23,16:24,HELLO,C#C#C#", "22:15,23:59,WORLD,CCC"]), "(None)")
+print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:02,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]),"(None)")
+print(solution(	"ABC", ["20:00,23:00,HELLO,CDEFGAB", "19:00,23:00,WORLD,ABCDEF", "18:00,23:00,SSKY,ABCDABCD"]), "SSKY")
+print(solution(	"ABC", ["09:50,09:58,HELLO,C#DEFGAB", "09:50,09:53,WORLD,ABCDEF#"]), "WORLD")
