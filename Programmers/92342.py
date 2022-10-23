69a4dcd99e0e9246389b721edcebcf543ea9dd5b
unknown <deltahackall@gmail.com>
2022-10-23 23:18:47 +0900

    Solved

--- Programmers/92342.py
+++ Programmers/92342.py
@@ -1,7 +1,11 @@
 from pprint import pprint
-
+from collections import Counter
+from functools import cmp_to_key
 def solution(n, info):
     answer = []
+
+    def compare():
+        pass
 
     def calculate(i, lion):
         # print(lion)
@@ -10,9 +14,9 @@ def solution(n, info):
         # print(f"index: {i}, lion's arrow: {lion}")
         if remainarrow == 0:
             # print("remain arrow is 0")
-            lionscore = sum([10 - i for i in range(11) if lion[i] > info[i]])
-            targetscore = sum([10 - i for i in range(11) if lion[i] < info[i]])
-            if lionscore > targetscore:
+            lionscore = sum([10 - idx for idx in range(11) if lion[idx] > info[idx]])
+            apeachscore = sum([10 - idx for idx in range(11) if lion[idx] <= info[idx] and info[idx]>0])
+            if lionscore > apeachscore:
                 # print(f"{lion} arrow score is {lionscore}")
                 answer.append((lion, lionscore))
             return
@@ -24,13 +28,17 @@ def solution(n, info):
     calculate(0, [0 for _ in range(11)])
     # pprint(answer)
     if len(answer)==0: return [-1]
-    else: return sorted(answer, key = lambda x : (-x[1], x[0]))[0][0]
+    else: 
+        return sorted(answer, key = lambda x : (-x[1], x[0]))[0][0]
+
+
+# print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]), [0, 2, 2, 0, 1, 0, 0, 0, 0, 0, 0])
+# print(solution(	1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ), 	[-1])
+# print(solution(	9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]), [1, 1, 2, 0, 1, 2, 2, 0, 0, 0, 0])
+# print(solution(	10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]), [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 2])
 
 # print(solution(1, [0,0,1,0,0,0,0,0,0,0,0,0]), [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 # print(solution(2, [0,0,1,1,0,0,0,0,0,0,0,0]), [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 # print(solution(3, [1,0,1,0,1,0,0,0,0,0,0,0]), [2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-# print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]), [0, 2, 2, 0, 1, 0, 0, 0, 0, 0, 0])
-# print(solution(	1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ), 	[-1])
-# print(solution(	9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]), [1, 1, 2, 0, 1, 2, 2, 0, 0, 0, 0])
-print(solution(	10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]), [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 2])
+# print(solution(3, [2,1,0,0,0,0,0,0,0,0,0,0]), [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])
 # print(solution(10, [1,0,1,1,1,1,1,1,1,1,1]))
