899423d1be4aa1b6413e6cb1a5a92c3f57fc666b
unknown <deltahackall@gmail.com>
2022-11-06 08:26:48 +0900

    In progress

--- Programmers/12905.py
+++ Programmers/12905.py
@@ -30,49 +30,22 @@ def solution2(board):
 
     return answer
 
-def solution3(board):
-    answer = 0
-    dic = {(str(x)+str(y))*2: True if board[x][y] == 1 else False for x in range(len(board)) for y in range(len(board[0]))}
-    print(dic)
-    def isSquare(lu, rd):
-        print(lu, rd)
-        try:
-            return dic["".join(map(str, lu)) + "".join(map(str, rd))]
-        except:
-            print(lu, rd)
-            if lu[0]!=rd[0] and rd[0]-lu[0] == rd[1]-lu[1]:
-                mid = int((rd[0]-lu[0])/2)
-                print((lu, [mid, mid]), ([lu[0], mid+1], [mid, rd[1]]), ([mid+1, lu[1]], [rd[0], mid]), ([mid+1, mid+1], rd))
-                dic["".join(map(str, lu)) + "".join(map(str, rd))] = isSquare(lu, [mid, mid]) and isSquare([lu[0], mid+1], [mid, rd[1]]) and isSquare([mid+1, lu[1]], [rd[0], mid]) and isSquare([mid+1, mid+1], rd)
-            elif rd[0]-lu[0] > rd[1]-lu[1]:
-                dic["".join(map(str, lu)) + "".join(map(str, rd))] = isSquare(lu, [lu[0]+ abs(lu[0]-rd[0]) -1, rd[1]]) and isSquare([lu[0]+ abs(lu[0]-rd[0]), lu[1]], rd)
-            else:
-                dic["".join(map(str, lu)) + "".join(map(str, rd))] = isSquare(lu, [rd[0], rd[1] + abs(lu[1]-rd[1]) -1]) and isSquare([lu[0], rd[1] + abs(lu[1]-rd[1])], rd)
-            return dic["".join(map(str, lu)) + "".join(map(str, rd))]
-
-    windowsize = 2; maxwdsize = min(len(board), len(board[0]))
-    while windowsize <= maxwdsize:
-        for i in range(len(board)-windowsize+1):
-            for j in range(len(board[0])-windowsize+1):
-                # print([i, j], [i+windowsize-1, j+windowsize-1])
-                if isSquare([i, j], [i+windowsize-1, j+windowsize-1]):
-                    answer = windowsize ** 2
-        windowsize += 1
-
-    return answer
-
-def solution(board): # Dynamic Programming & Binary search
+def solution3(board): # Dynamic Programming & Binary search
     dic = {}
     def issquare(ordtuple: tuple) -> bool:
+        if dic.get(ordtuple, None):
+            return dic[ordtuple]
+
         lu = [ordtuple[0], ordtuple[1]]
         rd = [ordtuple[2], ordtuple[3]]
 
+        if board[lu[0]][lu[1]] == 0 or board[rd[0]][rd[1]] == 0:
+            dic[ordtuple] = False
+            return dic[ordtuple]
+
         if lu == rd:
-            if dic.get(ordtuple, None):
-                return dic[ordtuple]
-            else:
-                dic[ordtuple] = True if board[lu[0]][lu[1]] == 1 else False
-                return dic[ordtuple]
+            dic[ordtuple] = True if board[lu[0]][lu[1]] == 1 else False
+            return dic[ordtuple]
 
         mid = int((rd[0]-lu[0])/2)
 
@@ -84,7 +57,7 @@ def solution(board): # Dynamic Programming & Binary search
             # print((lu[0],lu[1],lu[0]+mid,lu[1]+mid),(lu[0],lu[1]+mid+1,lu[0]+mid,rd[1]),(lu[0]+mid+1,lu[1],rd[0],lu[1]+mid),(lu[0]+mid+1,lu[1]+mid+1,rd[0],rd[1]))
             # dic[ordtuple] = issquare((lu[0],lu[1],lu[0]+mid,lu[1]+mid)) and issquare((lu[0],lu[1]+mid+1,lu[0]+mid,rd[1])) and issquare((lu[0]+mid+1,lu[1],rd[0],lu[1]+mid)) and issquare((lu[0]+mid+1,lu[1]+mid+1,rd[0],rd[1]))
         else:
-            if any(issquare(x)==False for x in [(lu[0],lu[1],lu[0]+mid,lu[1]+mid),(lu[0],lu[1]+mid,lu[0]+mid,rd[1]),(lu[0]+mid,lu[1],rd[0],lu[1]+mid),(lu[0]+mid,lu[1]+mid,rd[0],rd[1])]):
+            if any(issquare(x)==False for x in [(lu[0],lu[1],lu[0]+mid,lu[1]+mid),(lu[0],lu[1]+mid,lu[0]+mid,rd[1]),(lu[0]+mid,lu[1],rd[0],lu[1]+mid),(lu[0]+mid+1,lu[1]+mid+1,rd[0],rd[1])]):
                 dic[ordtuple] = False
             else:
                 dic[ordtuple] = True
@@ -99,17 +72,59 @@ def solution(board): # Dynamic Programming & Binary search
                     return True
         return False
 
-    start = 0; end = max(len(board), len(board[0])); mid = 0
+    start = 1; end = max(len(board), len(board[0]))+1; mid = 0
     while True:
         mid = int((start+end) / 2)
-        if start >= end:
+        if start == end:
             # print(dic)
-            return (mid) ** 2
+            return (start-1) ** 2
         else:
             if check(mid):
                 start = mid+1
             else:
                 end = mid
+
+def solution(board):
+    dic = {}
+    def rowsum(row, col1, col2):
+        if dic.get((row, col1, col2), None):
+            return dic.get((row, col1, col2))
+
+        total = 0
+        stack = [(col1, col2)]
+        while stack:
+            c1, c2 = stack.pop()
+            if dic.get((row, c1, c2), None) is not None:
+                total += dic[(row, c1, c2)]
+            elif c1 == c2:
+                dic[(row, c1, c2)] = board[row][c1]
+                total += dic[(row, c1, c2)]
+            else:
+                mid = int((c1+c2) / 2)
+                stack.extend([(c1, mid), (mid+1, c2)])
+        
+        dic[(row, col1, col2)] = total
+        return total
+
+    def issquare(ordtuple: tuple) -> bool:
+        for row in range(ordtuple[0], ordtuple[2]+1):
+            if sum(board[row][ordtuple[1]:ordtuple[3]+1]) != ordtuple[3]-ordtuple[1]+1:
+                return False
+        else:
+            return True
+
+    maxlen = 0
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            poslen = min(len(board)-i, len(board[0])-j)
+            if poslen <= maxlen:
+                continue
+            else:
+                if issquare((i, j, i+poslen-1, j+poslen-1)):
+                    maxlen = poslen
+                
+    return maxlen ** 2
     
 print(solution(	[[0, 0], [1, 1]] ), 1)
 print(solution(	[[0, 0, 1, 1], [1, 1, 1, 1]] ), 4)
@@ -120,4 +135,7 @@ print(solution(	[[1]*10]*10 ), 10**2)
 print(solution(	[[1]*100]*100 ), 100**2)
 print(solution(	[[1]*200]*200 ), 200**2)
 print(solution(	[[1]*500]*500 ), 500**2)
+print(solution(	[[1]*500]*1000 ), 500**2)
 print(solution(	[[1]*1000]*1000 ), 1000**2)
+print(solution(	[[0]*1000]*1000 ), 0)
+print(solution(	[[[1,0]*500], [[0,1]*500]]*500 ), 1)
