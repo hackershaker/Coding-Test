68af66243e73cd8015cec45fc92e563438ff2c8c
unknown <deltahackall@gmail.com>
2022-11-23 23:09:11 +0900

    Solved

---
+++ Baekjoon/11718.py
@@ -0,0 +1,11 @@
+def solution():
+    while True:
+        try:
+            string = input()
+            print(string)
+        except:
+            return
+
+
+if __name__=="__main__":
+    solution()
