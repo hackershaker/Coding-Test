5f400821fc76c2c9417cc9206dff17714b0000b1
unknown <deltahackall@gmail.com>
2022-11-28 09:33:03 +0900

    In progress

--- Programmers/12971.py
+++ Programmers/12971.py
@@ -1,5 +1,4 @@
-from collections import deque
-from heapq import heapify, heappop, heappush
+from heapq import heappop, heappush
 from itertools import count
 
 
@@ -15,42 +14,46 @@ def solution(sticker):
         heappush(
             heap, [sticker[i - 1] + sticker[(i + 1) % len(sticker)], -sticker[i], i, id]
         )
-        entryFinder[i] = id
+        entryFinder[i] = [id, '']
 
     while heap:
+        # print(heap)
+        # print(entryFinder)
         entry = heappop(heap)
-        if entryFinder[entry[2]] == REMOVED:
-            del entryFinder[entry[2]]
+        if entryFinder[entry[2]][-1] == REMOVED:
             continue
-        print(heap)
+        
         answer -= entry[1]  
         idx = entry[2]
         sticker[idx - 1], sticker[idx], sticker[(idx + 1) % len(sticker)] = 0, 0, 0
+        entryFinder[idx][-1] = REMOVED
         (
-            entryFinder[(idx - 1 + len(sticker)) % len(sticker)],
-            entryFinder[(idx + 1) % len(sticker)],
+            entryFinder[(idx - 1 + len(sticker)) % len(sticker)][-1],
+            entryFinder[(idx + 1) % len(sticker)][-1],
         ) = (REMOVED, REMOVED)
         (
-            entryFinder[(idx - 2 + len(sticker)) % len(sticker)],
-            entryFinder[(idx + 2) % len(sticker)],
+            entryFinder[(idx - 2 + len(sticker)) % len(sticker)][-1],
+            entryFinder[(idx + 2) % len(sticker)][-1],
         ) = (REMOVED, REMOVED)
 
-        id = next(counter)
-        heappush(
-            heap, [sticker[idx - 3] + sticker[idx - 1], -sticker[idx - 2], idx - 2, id]
-        )
-        entryFinder[(idx - 2 + len(sticker)) % len(sticker)] = id
+        if sticker[idx-2] != 0:
+            id = next(counter)
+            heappush(
+                heap, [sticker[idx - 3] + sticker[idx - 1], -sticker[idx - 2], (idx - 2 + len(sticker)) % len(sticker), id]
+            )
+            entryFinder[(idx - 2 + len(sticker)) % len(sticker)] = [id, '']
 
-        id = next(counter)
-        heappush(
-            heap,
-            [
-                sticker[(idx + 1) % len(sticker)] + sticker[(idx + 3) % len(sticker)],
-                -sticker[(idx + 2) % len(sticker)],
-                (idx + 2) % len(sticker),
-                id,
-            ],
-        )
-        entryFinder[(idx + 2) % len(sticker)] = id
+        if sticker[(idx + 2) % len(sticker)] != 0:
+            id = next(counter)
+            heappush(
+                heap,
+                [
+                    sticker[(idx + 1) % len(sticker)] + sticker[(idx + 3) % len(sticker)],
+                    -sticker[(idx + 2) % len(sticker)],
+                    (idx + 2) % len(sticker),
+                    id,
+                ],
+            )
+            entryFinder[(idx + 2) % len(sticker)] = [id, '']
 
     return answer
