402dbf144b43d6eba5c622d360b96c8202ca8028
unknown <deltahackall@gmail.com>
2022-12-06 23:24:21 +0900

    In progress

--- Programmers/67259.py
+++ Programmers/67259.py
@@ -1,28 +1,56 @@
 from collections import deque
-def solution(board):
+def solution2(board):
     answer = float("inf")
 
-    stack = deque([ [[(0,0)],0] ])
+    stack = [ [[(0,0)],0] ]
     while stack:
-        path, cost = stack.popleft()
+        path, cost = stack.pop()
         if cost > answer: continue
 
         p = path[-1]
         if p == (len(board)-1, len(board[0])-1):
-            print(p, cost)
             answer = min(answer, cost)
-        board[p[0]][p[1]] = 1
 
         u,d,r,l = (1,0), (-1,0), (0,1), (0,-1)
         
-        for a in u,d,r,l:
-            if 0 <= p[0]+a[0] < len(board) and 0 <= p[1]+a[1] < len(board[0]) and board[p[0]+a[0]][p[1]+a[1]] != 1:
+        for a in d,r,l,u:
+            if 0 <= p[0]+a[0] < len(board) and 0 <= p[1]+a[1] < len(board[0]) and (p[0]+a[0], p[1]+a[1]) not in path and board[p[0]+a[0]][p[1]+a[1]] != 1:
                 if len(path) == 1:
-                    stack.append([path[-1:] + [(p[0]+a[0], p[1]+a[1])], cost+100])
+                    stack.append([path + [(p[0]+a[0], p[1]+a[1])], cost+100])
                 else:
-                    if len(path) > 1 and path[-2][0]-path[-1][0] == -a[0] and path[-2][1]-path[-1][1] == -a[1]:
-                        stack.append([path[-1:] + [(p[0]+a[0], p[1]+a[1])], cost+100])
+                    if path[-2][0]-path[-1][0] == -a[0] and path[-2][1]-path[-1][1] == -a[1]:
+                        stack.append([path + [(p[0]+a[0], p[1]+a[1])], cost+100])
                     else:
-                        stack.append([path[-1:] + [(p[0]+a[0], p[1]+a[1])], cost+600])
+                        stack.append([path + [(p[0]+a[0], p[1]+a[1])], cost+600])
                     
+    return answer
+
+from collections import deque, defaultdict
+def solution(board):
+    n, m = len(board), len(board[0])
+    dic = defaultdict(list)
+    dic[(1,0)].append([[(0,0),(1,0)], 100])
+    dic[(0,1)].append([[(0,0),(0,1)], 100])
+    stack = deque([(1,0),(0,1)])
+    while stack:
+        node = stack.popleft()
+        print(dic[node])
+        u,d,r,l = (1,0), (-1,0), (0,1), (0,-1)
+        
+        for a in d,r,l,u:
+            if 0 <= node[0]+a[0] < n and 0 <= node[1]+a[1] < m and board[node[0]+a[0]][node[1]+a[1]] != 1:
+                if (node[0]+a[0], node[0]+a[1]) not in dic:
+                    stack.append((node[0]+a[0], node[0]+a[1]))
+                else:
+                    for nextnode in dic[(node[0], node[1])]:
+                        path, cost = nextnode[0]
+
+                        if path[-2][0]-path[-1][0] == -a[0] and path[-2][1]-path[-1][1] == -a[1]:
+                            dic[(node[0]+a[0], node[1]+a[1])].append([path[-1] + [(node[0]+a[0], node[0]+a[1])], cost+100])
+                        else:
+                            dic[(node[0]+a[0], node[1]+a[1])].append([path[-1] + [(node[0]+a[0], node[0]+a[1])], cost+600])
+
+    answer = 0
+    for total in dic[(n-1, m-1)]:
+        answer = min(answer, total)
     return answer
