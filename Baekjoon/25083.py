349cf89b73d47667761a680813e8511b42c1f4a3
unknown <deltahackall@gmail.com>
2023-01-01 11:14:51 +0900

    Solved

---
+++ Baekjoon/25083.py
@@ -0,0 +1,14 @@
+# string 안에 ', "가 들어갔을 때 처리에 유의할 것
+
+def solution():
+    s = ['         ,r\'"7',
+    'r`-_   ,\'  ,/',
+    ' \. ". L_r\'',
+    '   `~\/',
+    '      |',
+    '      |',]
+    return s
+
+if __name__ == "__main__":
+    answer = solution()
+    for s in answer: print(s)
