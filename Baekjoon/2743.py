1f98105a3cc4f0bc580e2c8d43a8e6c57fd2bed9
DY <deltahackall@gmail.com>
2023-04-14 23:04:33 +0900

    Solved

---
+++ Baekjoon/2743.py
@@ -0,0 +1,12 @@
+import sys
+
+
+class Solution:
+    def solution(self):
+        string = sys.stdin.readline().strip()
+        print(len(string))
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.solution()
