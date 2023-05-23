4a1607c3f01e96c01fa3029558e98a3672dfcaaf
DY <deltahackall@gmail.com>
2023-05-24 08:31:57 +0900

    Solved

---
+++ Baekjoon/p10101.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if (a == 60 && b == 60 && c == 60)
+    {
+        cout << "Equilateral" << endl;
+    }
+    else if (a + b + c != 180)
+    {
+        cout << "Error" << endl;
+    }
+    else if (a == b || b == c || c == a)
+    {
+        cout << "Isosceles" << endl;
+    }
+    else
+    {
+        cout << "Scalene" << endl;
+    }
+    return 0;
+}
